9afe277c1c9a87f9a93ce5bdf4296325b478a61f
AleksandrKerov <kerovav@mail.ru>
2020-12-06 11:27:54 +0300

    HW6.Final

--- src/Lesson6/Animals/Animal.java
+++ src/Lesson6/Animals/Animal.java
@@ -13,24 +13,16 @@ public abstract class Animal {
         quantityOfAnimals++;
     }
 
-    public boolean run(int distance) {
-        return maxRun >= distance;
-    }
-
-    public boolean swim(int distance) {
-        return maxSwim >= distance;
-    }
-
-    public void running(int distnace){
-        if(maxRun>= distnace){
+    public void running(int distnace) {
+        if (maxRun >= distnace) {
             System.out.printf("%s has run %d\n", name, distnace);
         } else {
             System.out.printf("%s has not run %d. Maximum distance is %d\n", name, distnace, maxRun);
         }
     }
 
-    public void swimming(int distnace){
-        if(maxSwim>= distnace){
+    public void swimming(int distnace) {
+        if (maxSwim >= distnace) {
             System.out.printf("%s has swum %d\n", name, distnace);
         } else {
             System.out.printf("%s has not swum %d. Maximum distance is %d\n", name, distnace, maxSwim);
--- src/Lesson6/Main.java
+++ src/Lesson6/Main.java
@@ -17,29 +17,13 @@ public class Main {
                 new Dog("Rex", 500, 10)
         };
 
-        //1 вариант
-        for (int i = 0; i < animals.length; i++) {
-            System.out.printf("%s tries to run %d meters. Did it manage? %b\n", animals[i].name, distanceToRun, animals[i].run(distanceToRun));
-            System.out.printf("%s tries to swim %d meters. Did it manage? %b\n\n", animals[i].name, distanceToRun, animals[i].swim(distanceToSwim));
-        }
-
-        //2 вариант
         for (int i = 0; i < animals.length; i++) {
             animals[i].running(distanceToRun);
             animals[i].swimming(distanceToSwim);
         }
-        //какой вариант лучше? Слышал такое мнение, что животное не должно уметь само что-то говорить о своих способностях :)
 
-        //Хотел сделать метод printQuantity, в который передается Геттер и который бы писал "Количество *** Cat.getQuantityOfCats"
-        //Но как вместо *** назначить наименование класса?
         System.out.println("Quantity of animals is " + Animal.getQuantityOfAnimals());
         System.out.println("Quantity of cats is " + Cat.getQuantityOfCats());
         System.out.println("Quantity of dogs is " + Dog.getQuantityOfDogs());
-
-//        printQuantity(Cat.getQuantityOfCats());
     }
-
-//    public static void printQuantity(int quantity){
-//        System.out.printf("Quantity of %s is %d", КЛАСС, quantity);
-//    }
 }
